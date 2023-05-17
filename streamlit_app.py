66ee3ea482a5532ad455568f307029b3cf47ce96
hxh103 <87348117+hxh103@users.noreply.github.com>
2023-05-17 22:10:42 +1000

    filter data

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,7 @@ import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-st.multiselect("Pick some fruit: ", list(my_fruit_list.index), ['Avocado','Strawberries'])
-st.dataframe(my_fruit_list)
+fruits_selected = st.multiselect("Pick some fruit: ", list(my_fruit_list.index), ['Avocado','Strawberries'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+st.dataframe(fruits_to_show)
