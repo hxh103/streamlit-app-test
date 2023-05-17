5f8eaf1132c4890a22ca12dfc7cf65c08a270202
hxh103 <87348117+hxh103@users.noreply.github.com>
2023-05-17 22:09:18 +1000

    default values

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-st.multiselect("Pick some fruit: ", list(my_fruit_list.index))
+st.multiselect("Pick some fruit: ", list(my_fruit_list.index), ['Avocado','Strawberries')
 st.dataframe(my_fruit_list)
