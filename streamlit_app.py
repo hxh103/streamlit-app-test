f23624c9fc5f768b7f77b3302985b5f033043ee3
hxh103 <87348117+hxh103@users.noreply.github.com>
2023-05-17 22:06:14 +1000

    multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ st.header('header')
 st.text('text')
 
 st.text('🥣 🥗 🐔 🥑🍞')
-
+st.header('🥣 🥗 Build your own fruit smoothie 🐔 🥑🍞')
 import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+
+st.multiselect("Pick some fruit: ", list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
